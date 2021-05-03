2f6aa416fc982c5a156193545a6f9872bb09202a
shaakya-weerasundera <68537364+shaakya-weerasundera@users.noreply.github.com>
2021-05-03 17:05:20 +0930

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,5 +1,5 @@
 /* Update Code for users and projects, a bit unsure of SELECTION ones that is all. */
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, projects;
 
 USE users_db;
 
@@ -12,3 +12,9 @@ CREATE TABLE users (
 	PRIMARY KEY (user_id)
 )ENGINE=InnoDB, DEFAULT CHARACTER SET utf8;
 
+CREATE TABLE projects (
+	project_id INT(5) NOT NULL AUTO_INCREMENT,
+	project_name VARCHAR(30) NOT NULL,
+	project_description VARCHAR(100) NOT NULL,
+	PRIMARY KEY (project_id)
+)ENGINE=InnoDB, DEFAULT CHARACTER SET utf8;
