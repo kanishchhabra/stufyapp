60b4d9399ff1372a8fb19712fdffeefea4d775de
shaakya-weerasundera <68537364+shaakya-weerasundera@users.noreply.github.com>
2021-05-03 17:01:11 +0930

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,18 +1,14 @@
 /* Update Code for users and projects, a bit unsure of SELECTION ones that is all. */
-DROP TABLE IF EXISTS users, projects;
+DROP TABLE IF EXISTS users;
 
-CREATE TABLE users(
-	user_id INT UNSIGNED NOT NULL,
-	firstName VARCHAR(20) NOT NULL,
-	lastName VARCHAR(20) NOT NULL,
-	DOB DATETIME NOT NULL,
-	Email VARCHAR(35) NOT NULL,
-	Bio VARCHAR(30) NOT NULL
+USE users_db;
+
+CREATE TABLE users (
+	user_id INT(5) NOT NULL AUTO_INCREMENT,
+	firstName VARCHAR(15) NOT NULL,
+	lastName VARCHAR(25) NOT NULL,
+	DOB DATE NOT NULL,
+	Email VARCHAR(30) NOT NULL,
+	PRIMARY KEY (user_id)
 )ENGINE=InnoDB, DEFAULT CHARACTER SET utf8;
-
-CREATE TABLE projects(
-	user_id INT UNSIGNED NOT NULL,
-	Project_Name VARCHAR(20) NOT NULL,
-	Project_Description VARCHAR(20) NOT NULL
-);
 
