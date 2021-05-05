e12470f9ff81e2cbc5b264d19df245bee6c5ccc3
kanishchhabra <s316921@students.cdu.edu.au>
2021-05-06 00:05:37 +0930

    deleted misc file

--- db/load-csv.sql
+++ db/load-csv.sql
@@ -1,3 +1,4 @@
+/*Change the path and database accordingly and run this file extrnally or after logging into MySQL*/
 LOAD DATA INFILE 'C:/Users/kanis/OneDrive/Documents/HIT326-StufyApp/db/csv/qualifications.csv'
 INTO TABLE qualification
 FIELDS TERMINATED BY ',' 
