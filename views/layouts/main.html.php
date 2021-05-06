63489cfcee0761cdf489723d707f5ec4b5a6dcfa
shaakya-weerasundera <68537364+shaakya-weerasundera@users.noreply.github.com>
2021-05-06 16:23:29 +0930

    Login link update
    Send the link to the login page. Will try the functionality now

--- views/layouts/main.html.php
+++ views/layouts/main.html.php
@@ -5,7 +5,7 @@ require $_SERVER['DOCUMENT_ROOT'] . "/views/components/header.html.php";
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio ipsam beatae, magnam odio deserunt ab ex asperiores totam inventore tempora rerum quia nam cupiditate libero eveniet adipisci cum voluptas provident?</p>
 <ul>
     <li><a href=<?php echo HTML_LAYOUTS_PATH . 'registration.html.php'; ?>>Registration</a></li>
-    <li><a href="">Login</a></li>
+    <li><a href=<?php echo HTML_LAYOUTS_PATH . 'login.html.php'; ?>>Login</a></li>
 </ul>
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . "/views/components/footer.html.php";
