91e34cd038c1baa7715d03c61ccce1de11f20e84
shaakya-weerasundera <68537364+shaakya-weerasundera@users.noreply.github.com>
2021-05-06 16:25:35 +0930

    Basic login
    Login page. Will do functionality and form next

---
+++ views/layouts/login.html.php
@@ -0,0 +1,14 @@
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . "/views/components/header.html.php";
+?>
+
+<h1>Log in</h1>
+
+
+<?php
+include COMPONENTS_PATH . "/login_form.html.php";
+?>
+
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . "/views/components/footer.html.php";
+?>
