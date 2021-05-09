70639606357c158512b262bdb7ea1e2a3bd05a0d
kanishchhabra <s316921@students.cdu.edu.au>
2021-05-09 18:29:25 +0930

    - removed conflicts.

--- views/layouts/login.html.php
+++ views/layouts/login.html.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . "/views/components/header.html.php";
 ?>
@@ -12,19 +11,3 @@ include COMPONENTS_PATH . "/login_form.html.php";
 ?>
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . "/views/components/footer.html.php";
-=======
-<?php
-require $_SERVER['DOCUMENT_ROOT'] . "/views/components/header.html.php";
-?>
-
-<h1>Log in</h1>
-
-
-<?php
-include COMPONENTS_PATH . "/login_form.html.php";
-?>
-
-<?php
-require $_SERVER['DOCUMENT_ROOT'] . "/views/components/footer.html.php";
->>>>>>> 91e34cd038c1baa7715d03c61ccce1de11f20e84
-?>
