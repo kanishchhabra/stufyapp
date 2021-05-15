dec34b4a47545c80e5fc14ff96c35bf7a0ad8cce
shaakya-weerasundera <68537364+shaakya-weerasundera@users.noreply.github.com>
2021-05-15 21:01:50 +0930

    Add students html template

---
+++ views/layouts/add_students.html.php
@@ -0,0 +1,10 @@
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . "/views/components/header.html.php";
+
+?>
+
+<h1>Add Students</h1>
+
+<?php
+    include COMPONENTS_PATH . "/add_students._form.html.php.";
+?>
