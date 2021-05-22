c5bf310bc738c11926a44763bd7c6cc84c26b776
shaakya-weerasundera <68537364+shaakya-weerasundera@users.noreply.github.com>
2021-05-22 21:10:09 +0930

    form for filter
    Add form HTML

---
+++ views/content/filter_form.html.php
@@ -0,0 +1,44 @@
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . "/controller/paths.php";
+?>
+
+<form action=<?php echo HTML_CONTROLLER_PATH . 'new.php' ?> method='get'>
+<label for='discipline'>Discipline</label><br>
+    <select name='discipline' id='discipline'>
+        <?php
+        $table_name = 'discipline';
+        $column_name = 'discipline_name';
+        include CONTROLLER_PATH . '/options_validation.php';
+        ?>
+    </select><br>
+<label for="university">University</label><br>
+<select name="university">
+<?php
+$table_name = 'university';
+$column_name = 'university_name';
+include CONTROLLER_PATH . '/options_validation.php';
+?>
+</select><br>
+<label for='study_year'>Study Year</label><br>
+    <select name='study_year' id='study_year'>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+    </select><br>
+    <label for='qualification'>Qualification</label><br>
+    <select name='qualification' id='qualification'>
+        <?php
+        $table_name = 'qualification';
+        $column_name = 'qualification_name';
+        include CONTROLLER_PATH . '/options_validation.php';
+        ?>
+    </select><br>
+    <button type='submit' name='apply'>Apply Filters</button>
+
+
+
+
+
+
+</form>
