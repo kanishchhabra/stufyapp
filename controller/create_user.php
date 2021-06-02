7dc12de9640bfc45deeb6c86f66a7942b056b198
shaakya-weerasundera <68537364+shaakya-weerasundera@users.noreply.github.com>
2021-06-02 13:22:36 +0930

    validation
    Add preg_match and move insert try catch to if statement with validation, if the user enters valid data.

--- controller/create_user.php
+++ controller/create_user.php
@@ -7,38 +7,49 @@ require DB_PATH . '/db.php';
 if (isset($_POST['create_user'])) {
     $salt = str_shuffle(SALT);
     $password = md5($salt . $_POST['password']);
-
-    try {
-        $query = "INSERT INTO student
-        (firstName, lastName, DOB, email, university, study_year, discipline, qualification, pass, salt)
-        VALUES
-        (:first_name, 
-        :last_name, 
-        :date_of_birth, 
-        :email, 
-        (SELECT university_name FROM university WHERE university_name = :university), 
-        :study_year,
-        (SELECT discipline_name FROM discipline WHERE discipline_name = :discipline), 
-        (SELECT qualification_name FROM qualification WHERE qualification_name = :qualification), 
-        :pass, 
-        :salt)";
-
-        $stmt = $db->prepare($query);
-        $binding = array(
-            'first_name' => $_POST['first_name'],
-            'last_name' => $_POST['last_name'],
-            'date_of_birth' => $_POST['date_of_birth'],
-            'email' => $_POST['email'],
-            'university' => strval($_POST['university']),
-            'study_year' => $_POST['study_year'],
-            'discipline' => strval($_POST['discipline']),
-            'qualification' => strval($_POST['qualification']),
-            'pass' => $password,
-            'salt' => $salt
-        );
-        $stmt->execute($binding);
-        header('Location:/');
-    } catch (PDOException $errors) {
-        echo $errors->getMessage();
+    
+    /* get the POST email */
+    $email = $_POST['email'];
+    
+    /* validate the email */
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+        echo "valid";
+        try {
+            $query = "INSERT INTO student
+            (firstName, lastName, DOB, email, university, study_year, discipline, qualification, pass, salt)
+            VALUES
+            (:first_name, 
+            :last_name, 
+            :date_of_birth, 
+            :email, 
+            (SELECT university_name FROM university WHERE university_name = :university), 
+            :study_year,
+            (SELECT discipline_name FROM discipline WHERE discipline_name = :discipline), 
+            (SELECT qualification_name FROM qualification WHERE qualification_name = :qualification), 
+            :pass, 
+            :salt)";
+    
+            $stmt = $db->prepare($query);
+            $binding = array(
+                'first_name' => $_POST['first_name'],
+                'last_name' => $_POST['last_name'],
+                'date_of_birth' => $_POST['date_of_birth'],
+                'email' => $_POST['email'],
+                'university' => strval($_POST['university']),
+                'study_year' => $_POST['study_year'],
+                'discipline' => strval($_POST['discipline']),
+                'qualification' => strval($_POST['qualification']),
+                'pass' => $password,
+                'salt' => $salt
+            );
+            $stmt->execute($binding);
+            header('Location:/');
+        } catch (PDOException $errors) {
+            echo $errors->getMessage();
+        }
+    } else {
+        echo "Invalid email!";
     }
+
+    
 }
